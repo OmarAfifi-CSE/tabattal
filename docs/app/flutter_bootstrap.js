87674541bc68adf279fe48045825c056cf45d4f4
Omar Afifi <omarafifi.cse@gmail.com>
2026-09-24 08:33:49 +0300

    build(release): bump version to v1.5.1+22 and update web release bundle

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4199993822" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3042950390" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
